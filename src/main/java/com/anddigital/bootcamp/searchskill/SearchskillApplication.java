0570060dd96a269a04c1ad35b2abcdd08490652b
vinod-venugopal <winod.qmar@gmail.com>
2019-04-12 15:57:24 +0100

    First commit to add project

---
+++ src/main/java/com/anddigital/bootcamp/searchskill/SearchskillApplication.java
@@ -0,0 +1,13 @@
+package com.anddigital.bootcamp.searchskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchskillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anddigital/bootcamp/searchskill/SearchskillApplicationTests.java
@@ -0,0 +1,16 @@
+package com.anddigital.bootcamp.searchskill;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchskillApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
