f464fa45642758eb1a265750bfd216b0ae057e0f
vinod-venugopal <winod.qmar@gmail.com>
2019-04-15 14:35:36 +0100

    Added swagger configuration for REST documentation

--- src/main/java/com/anddigital/bootcamp/searchskill/resource/SearchController.java
+++ src/main/java/com/anddigital/bootcamp/searchskill/resource/SearchController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +14,27 @@ import com.anddigital.bootcamp.searchskill.clientengagement.ClientEngagement;
 import com.anddigital.bootcamp.searchskill.clientengagement.SearchResultsNotFoundException;
 import com.anddigital.bootcamp.searchskill.repository.SearchRepository;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class SearchController {
 
 	@Autowired
 	private SearchRepository searchRepository;
 
+	/**
+	 * Search all the engagements handled by the club
+	 * @return list of client engagements
+	 */
 	@GetMapping(path="/search")
 	public List<ClientEngagement> searchAllEngagements(){
 		return searchRepository.findAll(new Sort("clientName"));
 	}
 
+	/**
+	 * Search by industry, capability, client or project name
+	 * @param searchText
+	 * @return list of client engagements
+	 */
 	@GetMapping(path="/search/{searchText}")
 	public List<ClientEngagement> searchBySearchText(@PathVariable String searchText){
 		String likeCapability = "%" + searchText + "%";
@@ -34,6 +45,11 @@ public class SearchController {
 		return searchResults;
 	}
 	
+	/**
+	 * Search by 
+	 * @param searchText
+	 * @return list of client engagements
+	 */
 	@GetMapping(path="/searchByPrediction/{searchText}")
 	public List<String> searchByPrediction(@PathVariable String searchText){
 		String likeCapability = "%" + searchText + "%";
